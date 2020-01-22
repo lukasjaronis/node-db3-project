5a9630ca07d7f598a74acae63ea360806f598b95
lukasjaronis <jaronisluke13@gmail.com>
2020-01-22 16:24:13 -0500

    find works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,15 @@
 const database = require('../db-config');
+
+module.exports = {
+    find, 
+    // findByID,
+    // findSteps,
+    // add,
+    // update,
+    // remove
+}
+
+function find() {
+    return database('schemes')
+    .select()
+}
