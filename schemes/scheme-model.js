0e88463743702b988ae80e7644f521b349650625
lukasjaronis <jaronisluke13@gmail.com>
2020-01-22 17:23:42 -0500

    everything seems to be working correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,14 +2,48 @@ const database = require('../db-config');
 
 module.exports = {
     find, 
-    // findByID,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
 }
 
 function find() {
     return database('schemes')
     .select()
+}
+
+function findById(id) {
+    return database("schemes")
+    .where({ id })
+    .first()
+}
+
+function findSteps(schemeId) {
+    return database("steps")
+        .join("schemes", "schemes.id", "steps.scheme_id")
+        .where({scheme_id: schemeId})
+        .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
+        .orderBy("steps.step_number")
+}
+
+async function add(data) {
+    const [id] = await database('schemes')
+    .insert(data)
+    return database('schemes')
+    .where({ id })
+    .first()
+}
+
+function update(change, id) {
+    return database('schemes')
+    .where({ id })
+    .update(change)
+}
+
+function remove(id) {
+    return database('schemes')
+    .where({ id })
+    .del()
 }
